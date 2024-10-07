6221a1f02148581be179e96d18905adf832327fa
jenitamary0321 <jenitamary0321@gmail.com>
2024-10-07 10:50:21 +0530

    first commit

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Nutrition = require('../models/nutrition.model'); // Adjust this path based on your folder structure
-// const authenticateJWT = require('../middleware/auth'); // Middleware for authentication, adjust the path
-
+const Nutrition = require('../models/nutrition.model');  
 // POST route to add nutrition data
 router.post('/', async (req, res) => {
   try {
